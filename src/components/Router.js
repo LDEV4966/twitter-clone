2ec356f75a2bcbe378bd5d322e6f114537fe767a
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2020-12-28 01:25:28 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); //true -> 로그인 성공, false -> 인증단계
+  // useState는 react에서 받아온다. 위는 hooks 이다.
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
