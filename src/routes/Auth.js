1cb30915bd40f7ab5dbfdf5691f30e6f18e2c0c5
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2020-12-29 17:51:01 +0900

    make form to sign in with Google,Github

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => (
+<div>
+    <form>
+        <input type="text" placeholder="Email" required/>
+        <input type="password" placeholder="Password" required/>
+        <input type="submit" value="Log In"/>
+    </form>
+    <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+    </div>
+
+</div>
+);
 
 export default Auth;
