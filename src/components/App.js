1726cdd9462386bebc9536317d1172d6404f3224
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-01-03 00:22:32 +0900

    Create and Read

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import {authService} from "fbase";
 function App() {
   const [init,setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false); 
-
+  const [userObj,setUserObj] = useState(null);
   //useEffect 사용 하지 않을시에 firebase의 db를 동기화 시키기도 전에
   // const [isLoggedIn, setIsLoggedIn] 값을 인식한다. 이것이 아래 함수를 쓴 이유다
   useEffect( () => {
     authService.onAuthStateChanged((user) =>{
       if(user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -19,7 +20,7 @@ function App() {
   } , []) // useEffect는 동기화라고 생각하자. 마운트,언마운트,props값 업데이트시 사용 됨.
   return (
   <>
-  {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "initializing..."}
+  {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "initializing..."}
   <footer>&copy; {new Date().getFullYear()} Twitter</footer>
   </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,67 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [cnt, setCnt] = useState(0); //Re rendering cnt
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const history = useHistory();
+
+  const getTweets = async () => {
+    let dbTweets = await dbService.collection("tweet").get();
+    setCnt(cnt + 1);
+    setTweets([]);
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const data = await dbService.collection("tweet").add({
+      text: tweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setTweet("");
+    getTweets();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  console.log(tweets);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={tweet}
+          type="text"
+          placeholder="what's on your mind"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div>
+            <h4>{tweet.text} </h4>
+          </div>
+        ))}
+      </div>
+      <span>rendering : {cnt} times..</span>
+    </div>
+  );
+};
 
 export default Home;
